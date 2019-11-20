dc9870a7e3fb3819eed512736795d9a055241ee9
Eli Hayun <eli@venus.lan>
2019-11-20 06:18:12 +0200

    fix hebrew translation

--- resources/views/vendor/mail/html/promotion_contact.blade.php
+++ resources/views/vendor/mail/html/promotion_contact.blade.php
@@ -1,12 +1,12 @@
 <table class="promotion" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
     <tr>
         <td align="right">
-            <strong>'ששם פרטי  : '</strong>{{$first_name}}<br>
-            <strong>'ששם משפחה  : '</strong>{{$last_name}}<br>
-            <strong>'שדוא"ל : '</strong>{{$email}}<br>
-            <strong>'שמכשיר טלפון : '</strong>{{$phone}}<br>
+            <strong>'שם פרטי  : '</strong>{{$first_name}}<br>
+            <strong>'שם משפחה  : '</strong>{{$last_name}}<br>
+            <strong>'דוא"ל : '</strong>{{$email}}<br>
+            <strong>'מכשיר טלפון : '</strong>{{$phone}}<br>
             @if(!empty($interested_in))
-            <strong>'שאינטרס לקוחות   : '</strong>{{$interested_in}}<br> 
+            <strong>'אינטרס לקוחות   : '</strong>{{$interested_in}}<br> 
  			@endif
  			 @if(!empty($interested_in))
             <strong>'הודעת לקוח  : '</strong>{{$msg}}<br> 
--- resources/views/vendor/mail/html/promotion_review.blade.php
+++ resources/views/vendor/mail/html/promotion_review.blade.php
@@ -1,9 +1,9 @@
 <table class="promotion" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
     <tr>
         <td align="right">
-            <strong>'ששם פרטי  : '</strong>{{$first_name}}<br>
-            <strong>'ששם משפחה  : '</strong>{{$last_name}}<br>
-            <strong>'שדוא"ל : '</strong>{{$email}}<br>
+            <strong>'שם פרטי  : '</strong>{{$first_name}}<br>
+            <strong>'שם משפחה  : '</strong>{{$last_name}}<br>
+            <strong>'דוא"ל : '</strong>{{$email}}<br>
             <strong>'כותרת המלצה  : '</strong>{{$title}}<br>
             <strong>'המלצה  : '</strong>{{$remark}}<br>
 

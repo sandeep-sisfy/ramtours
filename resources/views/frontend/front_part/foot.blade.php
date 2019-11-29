09ff401bc6846d6957f4aa182ac28cb0e21d828a
Eli Hayun <eli@venus.lan>
2019-11-29 13:24:23 +0200

    preventing right click

--- resources/views/frontend/front_part/foot.blade.php
+++ resources/views/frontend/front_part/foot.blade.php
@@ -20,6 +20,21 @@
   gtag('js', new Date());
 
   gtag('config', 'UA-38205015-1');
+  $(document).on({
+    "contextmenu": function (e) {
+      console.log("ctx menu button:", e.which);
+
+      // Stop the context menu
+      e.preventDefault();
+    },
+    "mousedown": function (e) {
+      console.log("normal mouse down:", e.which);
+    },
+    "mouseup": function (e) {
+      console.log("normal mouse up:", e.which);
+    }
+  });
+
 </script>
 @endif
 

f72737ba331c7672b317a90bbee471f7008ba292
Eli Hayun <eli@venus.lan>
2019-12-01 16:36:05 +0200

    adding payment check

--- resources/views/mobile/pages/payment_fail.blade.php
+++ resources/views/mobile/pages/payment_fail.blade.php
@@ -1,23 +1,11 @@
-@extends('mobile.home_main')
-@section('mobile_container')
 <section class="rt_error">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-8">
-              <img src="{{url('assets/mobile')}}/images/payment-error.png" class="img-fluid" alt="Payment Error">
-              <h5>שגיאת תשלום </h5>
-              <p>מצטער ! לא ניתן לעבד את התשלום שלך. בבקשה נסה שוב</p>
-              </div>
-            </div>
-           </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8">
+        <img src="{{url('assets/mobile')}}/images/payment-error.png" class="img-fluid" alt="Payment Error">
+        <h5>שגיאת תשלום </h5>
+        <p>מצטער ! לא ניתן לעבד את התשלום שלך. בבקשה נסה שוב</p>
+      </div>
+    </div>
+  </div>
 </section>
-@endsection
-@section('rami_mobile_footer_js')
-@parent
-<script type="text/javascript">
-  $.ajax({
-    url: '{{ url("auto/setup_all_package_cost")}}',
-    type: 'GET',
-  })
-</script>
-@endsection
--- resources/views/mobile/pages/payment_success.blade.php
+++ resources/views/mobile/pages/payment_success.blade.php
@@ -1,23 +1,11 @@
-@extends('mobile.home_main')
-@section('mobile_container')
-  <section class="rt_error">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-8">
-              <img src="{{url('assets/mobile')}}/images/payment-success.png" class="img-fluid" alt="Payment Error">
-              <h5>שגיאת תשלום </h5>
-              <p>מצטער ! לא ניתן לעבד את התשלום שלך. בבקשה נסה שוב <br> אנא בדוק בדוא"ל שלך את אישור ההזמנה. </p>
-              </div>
-            </div>
-           </div>
-  </section>
-@endsection
-@section('rami_mobile_footer_js')
-@parent
-<script type="text/javascript">
-  $.ajax({
-    url: '{{ url("auto/setup_all_package_cost")}}',
-    type: 'GET',
-  })
-</script>
-@endsection
+<section class="rt_error">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <img src="{{url('assets/front/images/payment-success.png')}}" alt="Payment Successful">
+        <h5>עיסקה בוצעה בהצלחה </h5>
+        <p>תודה ! התשלום שלך התקבל בהצלחה <br> אנא בדוק בדוא"ל שלך את אישור ההזמנה. </p>
+      </div>
+    </div>
+  </div>
+</section>
